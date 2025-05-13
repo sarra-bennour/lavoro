a1893cd6f06fc3cf60e11dfadf15539e16ddd2c2
Sarra Ben Nour <sarra.bennour@esprit.tn>
2025-05-13 23:38:22 +0100

    Fix App.jsx 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, HashRouter, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, BrowserRouter, Route, Navigate, useLocation } from "react-router-dom";
 import SignUp from "./user/Signup";
 import SignIn from "./user/Signin";
 import Home from "./Home";
@@ -129,11 +129,12 @@ function App() {
 
   return (
     <GoogleOAuthProvider clientId="893053722717-a3eudc815ujr6ne3tf5q3dlrvkbmls6d.apps.googleusercontent.com">
-      <HashRouter>
+      <BrowserRouter>
         <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
         <Routes>
           {/* Public routes */}
-          <Route path="/" element={<Landing />} />
+          {/* <Route path="/" element={<Landing />} /> */}
+          <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
@@ -473,7 +474,7 @@ function App() {
 
 
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </GoogleOAuthProvider>
   );
 }
